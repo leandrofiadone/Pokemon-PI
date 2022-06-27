6af83127d856dea0d069dc79c3f9141c656c16f9
leandrofiadone <leanfiadone@gmail.com>
2022-06-27 16:26:03 -0300

    Deploy cors

--- api/src/app.js
+++ api/src/app.js
@@ -7,9 +7,9 @@ const routes = require('./routes/index.js');
 require('./db.js');
 
 const server = express();
-
+const cors = require('cors');
 server.name = 'API';
-
+server.use(cors());
 server.use(bodyParser.urlencoded({ extended: true, limit: '50mb' }));
 server.use(bodyParser.json({ limit: '50mb' }));
 server.use(cookieParser());
