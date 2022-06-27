e52522f99009c54c8bd91fd00c5f1eb5a6bd1a65
leandrofiadone <leanfiadone@gmail.com>
2022-06-27 16:36:56 -0300

    pokemonJs

--- api/src/routes/index.js
+++ api/src/routes/index.js
@@ -1,7 +1,7 @@
 const { Router } = require('express');
 // Importar todos los routers;
 // Ejemplo: const authRouter = require('./auth.js');
-const pokemonsRoutes = require("./pokemons.js");
+const pokemonsRoutes = require("./Pokemons.js");
 const typesRoutes = require("./types.js");
 
 const router = Router();
