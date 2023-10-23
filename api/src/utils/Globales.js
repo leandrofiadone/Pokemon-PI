ff9d0afbe02538cfca912726ed5ad817850728dd
Leandro Fiadone <leanfiadone@gmail.com>
2023-10-22 22:13:35 -0300

    cambio

--- api/src/utils/Globales.js
+++ api/src/utils/Globales.js
@@ -1,9 +1,9 @@
-const API_POKEMON = "https://pokeapi.co/api/v2/pokemon?limit=60"   //url con los primeros 40 pokemons
-const API_POKEMON_TYPE = "https://pokeapi.co/api/v2/type";
-const API_POKEMON_NAME_OR_ID = "https://pokeapi.co/api/v2/pokemon/";
+const API_POKEMON = "https://pokeapi.co/api/v2/pokemon?limit=60" //url con los primeros 40 pokemons
+const API_POKEMON_TYPE = "https://pokeapi.co/api/v2/type"
+const API_POKEMON_NAME_OR_ID = "https://pokeapi.co/api/v2/pokemon/"
 
 module.exports = {
-    API_POKEMON, 
-    API_POKEMON_TYPE,
-    API_POKEMON_NAME_OR_ID
+  API_POKEMON,
+  API_POKEMON_TYPE,
+  API_POKEMON_NAME_OR_ID
 }
